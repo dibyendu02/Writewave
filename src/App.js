a62f0019f9186d5f8f67c1179fb4be57f61e7349
Dibyendu Das <get2dibyendu@gmail.com>
2023-08-24 23:42:26 +0530

    final updates in json

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import { auth, db } from "./firebase-config";
 import { ViewPost } from './pages/ViewPost';
 import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'
 import Navbar from './components/Navbar';
-import About from './pages/About';
 import Profile from './pages/Profile';
 import Blogs from './pages/Blogs';
 import Footer from './components/Footer';
+import { ViewUser } from './pages/ViewUser';
 
 function App() {
   const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
@@ -37,16 +37,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home isAuth={isAuth}/>} />
         <Route path="/createpost" element={<CreatePost isAuth={isAuth}/>} />
-        <Route path="/login" element={<Login setIsAuth={setIsAuth}/>} />
-        <Route path="/:id" element={<ViewPost/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/blogs" element={<Blogs/>}/>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} isAuth={isAuth}/>} />
+        <Route path="/blogs/post/:id" element={<ViewPost/>}/>
+        <Route path="/blogs/user/:userid" element={<ViewUser/>}/>
+        <Route path="/profile/:id" element={<ViewPost/>}/>
+        <Route path="/profile" element={<Profile isAuth={isAuth} />}/>
+        <Route path="/blogs" element={<Blogs isAuth={isAuth} />}/>
       </Routes>
-      {/* <footer>
-        <p>Made with ❤️ by<a href="#">Dibyendu</a></p>
-      </footer> */}
-      <Footer/>
+      
     </Router>
   );
 }
